bab09ad02d2becb639912af601dcb0912dcf0f41
Tinnakorn Moreya <iam_rorrak@hotmail.com>
2016-08-20 08:30:57 +0700

    Delete useronline.php

--- useronline.php
+++
@@ -1,8 +0,0 @@
-<?php session_start(); 
-
-echo 'สวัสดี | '; 
-echo $_SESSION['login_true']; 
-echo '<div style="font-size:10px; color:#666;"> เวลา session :';
-echo date('Y/m/d H:i:s', time());
-echo '</div>';
- ?>
